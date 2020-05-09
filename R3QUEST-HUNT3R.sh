20227c8311b0c22298761431bd7e6293cc21bd70
im karthikeyan.v (cyber._.spartan) <33289300+karthi-the-hacker@users.noreply.github.com>
2020-05-09 12:22:59 +0530

    Add files via upload

---
+++ R3QUEST-HUNT3R.sh
@@ -0,0 +1,18 @@
+ #!/bin/bash
+
+clear
+./.banner.sh
+
+read -p "Enter file name: " fl
+read -p "Enter comany name: " comp
+mkdir ~/recon/$comp
+n=1
+cd ~/recon/$comp/
+mkdir req-maker
+cd req-maker
+while read line;do
+ echo " Domain $n : $line"
+ echo $line | tee -a $fl.txt
+ curl -i -s $line | tee $line.txt
+ n=$((n+1))
+done < $fl 
