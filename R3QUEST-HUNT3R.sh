d44a693a7bbcfc09d51882a6a2bf819f9790c08d
im karthikeyan.v (cyber._.spartan) <33289300+karthi-the-hacker@users.noreply.github.com>
2020-11-01 09:08:25 +0530

    Update R3QUEST-HUNT3R.sh

--- R3QUEST-HUNT3R.sh
+++ R3QUEST-HUNT3R.sh
@@ -5,14 +5,15 @@ clear
 
 read -p "Enter file name: " fl
 read -p "Enter company name: " comp
-mkdir ~/recon/$comp
+mkdir ~/recon/$comp/ &> /dev/null
 n=1
 cd ~/recon/$comp/
-mkdir req-hunter
+mkdir req-hunter &> /dev/null
 cd req-hunter
 while read line;do
  echo " Domain $n : $line"
  echo $line | tee -a $fl.txt
  curl -i -s $line | tee $line.txt
+ curl -i -s https://$line | tee htt_$line.txt
  n=$((n+1))
 done < $fl 
